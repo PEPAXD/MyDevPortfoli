0e3018674bcb99afbeeed0f18ca5e2f4bf839bea
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-02-05 03:30:00 -0300

    FirstCommit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="background">
+        <div className="container">
+          <h1>First-Commit</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
