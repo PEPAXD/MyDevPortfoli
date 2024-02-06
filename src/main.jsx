391cd3ac86b602474bd910a854394119e0d9c873
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-02-06 06:12:41 -0300

    backgroundPattern

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom/client";
 import App from "./Components/App.jsx";
 import Header from "./Components/Header.jsx";
 
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
